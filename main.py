1a91e0f63f54afff62e250626dbf7ce8132472b1
Brian Raderman <brian@irregularexpression.org>
2018-04-07 23:32:19 -0400

    Chapter 7: Diffuse materials

--- main.py
+++ main.py
@@ -15,14 +15,23 @@ def testImage():
 			ib = int(255.99 * col.B)
 			print("%d %d %d" % (ir, ig, ib))
 
+def randomInUnitSphere():
+	while True:
+		p = 2.0 * Vec3(random(), random(), random()) - Vec3(1, 1, 1)
+		if p.SquaredLength < 1.0:
+			break
+
+	return p
+
 def color(r, world):
-	bHit, rec = world.hit(r, 0.0, sys.float_info.max)
+	bHit, rec = world.hit(r, 0.001, sys.float_info.max)
 	if bHit:
-		return 0.5 * Vec3(rec.Normal.X+1, rec.Normal.Y+1, rec.Normal.Z+1)
+		target = rec.P + rec.Normal + randomInUnitSphere()
+		return 0.5 * color(Ray(rec.P, target - rec.P), world)
 	else:
 		unitDirection = Vec3.unitVector(r.Direction)
 		t = 0.5 * (unitDirection.Y + 1.0)
-		return (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
+		return (1.0 - t) * Vec3(1, 1, 1) + t * Vec3(0.5, 0.7, 1.0)
 
 def main():
 	nx = 200
@@ -31,8 +40,8 @@ def main():
 	print("P3\n%d %d\n255" % (nx, ny))
 
 	world = HitableList()
-	world.append(Sphere(Vec3(0.0, 0.0, -1.0), 0.5))
-	world.append(Sphere(Vec3(0.0, -100.5, -1.0), 100.0))
+	world.append(Sphere(Vec3(0, 0, -1), 0.5))
+	world.append(Sphere(Vec3(0, -100.5, -1), 100))
 
 	cam = Camera()
 
@@ -45,7 +54,8 @@ def main():
 				r = cam.getRay(u, v)
 				col += color(r, world)
 
-			col /= float(ns)	
+			col /= float(ns)
+			col = Vec3(math.sqrt(col.R), math.sqrt(col.G), math.sqrt(col.B))	
 			ir = int(255.99 * col.R)
 			ig = int(255.99 * col.G)
 			ib = int(255.99 * col.B)
